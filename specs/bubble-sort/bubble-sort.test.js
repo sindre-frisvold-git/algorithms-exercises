33eaff22bf531656adab7d707e995397f2f7979c
Sindre <sinfr91@gmail.com>
2022-06-12 22:03:47 +1200

    initial commit

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -16,8 +16,8 @@ function bubbleSort(nums) {
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
-  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
-  const sortedNums = bubbleSort(nums);
-  expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-});
+test.skip('bubble sort', function () {
+  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1]
+  const sortedNums = bubbleSort(nums)
+  expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+})

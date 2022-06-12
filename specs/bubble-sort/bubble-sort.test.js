f1cc854b3d8aa3a041a10a03ef68ca0b4caa2e40
Sindre <sinfr91@gmail.com>
2022-06-12 22:33:02 +1200

    incomplete solution

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -11,31 +11,32 @@
 */
 
 function bubbleSort(nums) {
-  // code goes 
+  // code goes
   // takes an array, compares 2 inecies, if the first is bigger, swap them
   // if no swaps has been made during the current run, exit
   let swapMade
-  while(swapMade = true){
+  while (swapMade === true) {
     swapMade = false
-    for(let i = 0; i < nums.length; i++){
-      if(nums[i] > nums[i+1]){
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > nums[i + 1]) {
         let temp = nums[i]
-        nums[i] = nums[i+1]
-        nums[i+1] = temp
+        nums[i] = nums[i + 1]
+        nums[i + 1] = temp
         swapMade = true
       }
+      console.log(nums)
     }
   }
   return nums
 }
 
-function swap(a, b){
-  const temp 
-}
+// function swap(a, b){
+//   const temp
+// }
 
 // unit tests
 // do not modify the below code
-test.skip('bubble sort', function () {
+test('bubble sort', function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1]
   const sortedNums = bubbleSort(nums)
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])

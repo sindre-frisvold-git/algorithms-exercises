d4774712cdc2dd00d3ddb4c4f7b90319832de179
Sindre <sinfr91@gmail.com>
2022-06-21 13:22:54 +1200

    remove completed solution

--- specs/arrays/linkedlist.test.js
+++ specs/arrays/linkedlist.test.js
@@ -24,120 +24,65 @@
 */
 
 class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-  push(value) {
-    const node = new Node(value);
-    this.length++;
-    if (!this.head) {
-      this.head = node;
-    } else {
-      this.tail.next = node;
-    }
-    this.tail = node;
-  }
-  pop() {
-    return this.delete(this.length - 1);
-  }
-  _find(index) {
-    if (index >= this.length) return null;
-    let current = this.head;
-    for (let i = 0; i < index; i++) {
-      current = current.next;
-    }
-
-    return current;
-  }
-  get(index) {
-    const node = this._find(index);
-    if (!node) return void 0;
-    return node.value;
-  }
-  delete(index) {
-    if (index === 0) {
-      const head = this.head;
-      if (head) {
-        this.head = head.next;
-      } else {
-        this.head = null;
-        this.tail = null;
-      }
-      this.length--;
-      return head.value;
-    }
-
-    const node = this._find(index - 1);
-    const excise = node.next;
-    if (!excise) return null;
-    node.next = excise.next;
-    if (!node.next) this.tail = node.next;
-    this.length--;
-    return excise.value;
-  }
+  // code goes here
 }
 
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+  // code goes here
 }
 
 // unit tests
 // do not modify the below code
-describe("LinkedList", function () {
+describe('LinkedList', function () {
   const range = (length) =>
-    Array.apply(null, { length: length }).map(Number.call, Number);
+    Array.apply(null, { length: length }).map(Number.call, Number)
   const abcRange = (length) =>
-    range(length).map((num) => String.fromCharCode(97 + num));
-  let list;
+    range(length).map((num) => String.fromCharCode(97 + num))
+  let list
 
   beforeEach(() => {
-    list = new LinkedList();
-  });
+    list = new LinkedList()
+  })
 
-  test("constructor", () => {
-    expect(list).toEqual(expect.any(LinkedList));
-  });
+  test('constructor', () => {
+    expect(list).toEqual(expect.any(LinkedList))
+  })
 
-  test("push", () => {
-    abcRange(26).map((character) => list.push(character));
-    expect(list.length).toEqual(26);
-  });
+  test('push', () => {
+    abcRange(26).map((character) => list.push(character))
+    expect(list.length).toEqual(26)
+  })
 
-  test("pop", () => {
-    abcRange(13).map((character) => list.push(character));
-    expect(list.length).toEqual(13);
-    range(10).map(() => list.pop());
-    expect(list.length).toEqual(3);
-    expect(list.pop()).toEqual("c");
-  });
+  test('pop', () => {
+    abcRange(13).map((character) => list.push(character))
+    expect(list.length).toEqual(13)
+    range(10).map(() => list.pop())
+    expect(list.length).toEqual(3)
+    expect(list.pop()).toEqual('c')
+  })
 
-  test("get", () => {
-    list.push("first");
-    expect(list.get(0)).toEqual("first");
-    list.push("second");
-    expect(list.get(1)).toEqual("second");
-    expect(list.get(0)).toEqual("first");
-    abcRange(26).map((character) => list.push(character));
-    expect(list.get(27)).toEqual("z");
-    expect(list.get(0)).toEqual("first");
-    expect(list.get(9)).toEqual("h");
-    list.pop();
-    expect(list.get(list.length - 1)).toEqual("y");
-  });
+  test('get', () => {
+    list.push('first')
+    expect(list.get(0)).toEqual('first')
+    list.push('second')
+    expect(list.get(1)).toEqual('second')
+    expect(list.get(0)).toEqual('first')
+    abcRange(26).map((character) => list.push(character))
+    expect(list.get(27)).toEqual('z')
+    expect(list.get(0)).toEqual('first')
+    expect(list.get(9)).toEqual('h')
+    list.pop()
+    expect(list.get(list.length - 1)).toEqual('y')
+  })
 
-  test("delete", () => {
-    abcRange(26).map((character) => list.push(character));
-    list.delete(13);
-    expect(list.length).toEqual(25);
-    expect(list.get(12)).toEqual("m");
-    expect(list.get(13)).toEqual("o");
-    list.delete(0);
-    expect(list.length).toEqual(24);
-    expect(list.get(0)).toEqual("b");
-  });
-});
+  test('delete', () => {
+    abcRange(26).map((character) => list.push(character))
+    list.delete(13)
+    expect(list.length).toEqual(25)
+    expect(list.get(12)).toEqual('m')
+    expect(list.get(13)).toEqual('o')
+    list.delete(0)
+    expect(list.length).toEqual(24)
+    expect(list.get(0)).toEqual('b')
+  })
+})

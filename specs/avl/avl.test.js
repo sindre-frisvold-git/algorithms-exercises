0e4fba273d5a3b1584e24f4b8f9a46552303deb0
Sindre <sinfr91@gmail.com>
2022-06-21 23:46:13 +1200

    add method node

--- specs/avl/avl.test.js
+++ specs/avl/avl.test.js
@@ -21,119 +21,144 @@
 
 */
 
+// class Tree {
+//   constructor() {
+//     this.root = null
+//   }
+//   add(value) {
+//     if (this.root === null) {
+//       this.root = new Node(value)
+//     } else {
+//       const path = []
+//       let depth = 0
+//       let current = this.root
+//       // check if value is greater or lower.
+//       // place in right or left if larger or smaller
+//       // if right or left does not exist, create and place value
+//       // else continue loop and add one to depth
+//       while (true) {
+//         path.push(current)
+//         if (current.value < value) {
+//           // go right
+//           if (current.right) {
+//             current = current.right
+//             depth++
+//           } else {
+//             current.right = new Node(value)
+//             break
+//           }
+//         } else {
+//           // go left
+//           if (current.left) {
+//             current = current.left
+//             depth++
+//           } else {
+//             current.left = new Node(value)
+//             break
+//           }
+//         }
+//       }
+//       this._checkBalance(path)
+//       // check for imbalance here
+//       // insert rotate logic here
+//     }
+//   }
+//   _checkBalance(path) {
+//     for (let i = path.length - 1; i >= 0; i--) {
+//       const depth = {}
+//       const node = path[i]
+//       console.log(node)
+//       if (node.left) depth.left = this._checkDepth(node.left)
+//       if (node.right) depth.right = this._checkDepth(node.right)
+//       // console.log(depth)
+//       if (depth.right > 1) {
+//         this._rotateRight(node)
+//         break
+//       }
+//       if (depth.left > 1) {
+//         this._rotateLeft(node)
+//         break
+//       }
+//       // if balance num is over 1 rotate
+//     }
+//     // start at end of path, check balance
+//     // ? recursive call
+//   }
+//   _rotateRight(node) {
+//     if (node.right.left) {
+//       const left = node.right.left.value
+//       node.right.left.value = node.right.value
+//       node.right.value = left
+
+//       node.right.right = node.right.left
+//       node.right.left = null
+//     }
+//     const temp = node.value
+//     node.value = node.right.value
+//     node.right.value = temp
+
+//     node.left = node.right
+//     node.right = node.right.right
+//     node.left.right = null
+
+//     // take node, swap value with first child
+//     // then set self as left or right child
+//     // take chi
+//   }
+
+//   _rotateLeft(node) {
+//     if (node.left.right) {
+//       const right = node.left.right.value
+//       node.left.right.value = node.left.value
+//       node.left.value = right
+
+//       node.left.left = node.left.right
+//       node.left.right = null
+//     }
+//     const temp = node.value
+//     node.value = node.left.value
+//     node.left.value = temp
+
+//     node.right = node.left
+//     node.left = node.left.left
+//     node.right.left = null
+//     // take node, swap value with first child
+//     // then set self as left or right child
+//     // take chi
+//   }
+//   _checkDepth(node) {
+//     // checks depth of a diven node
+//     if (!node.left && !node.right) return 1
+//     if (!node.left) return this._checkDepth(node.right) + 1
+//     if (!node.right) return this._checkDepth(node.left) + 1
+//     const left = this._checkDepth(node.left)
+//     const right = this._checkDepth(node.right)
+//     return left < right ? right + 1 : left + 1
+//   }
+//   toObject() {
+//     return this.root
+//   }
+// }
+
+// class Node {
+//   constructor(value) {
+//     this.value = value
+//     this.left = null
+//     this.right = null
+//   }
+//   // code also goes here
+// }
+
 class Tree {
   constructor() {
     this.root = null
   }
   add(value) {
-    if (this.root === null) {
+    if (!this.root) {
       this.root = new Node(value)
     } else {
-      const path = []
-      let depth = 0
-      let current = this.root
-      // check if value is greater or lower.
-      // place in right or left if larger or smaller
-      // if right or left does not exist, create and place value
-      // else continue loop and add one to depth
-      while (true) {
-        path.push(current)
-        if (current.value < value) {
-          // go right
-          if (current.right) {
-            current = current.right
-            depth++
-          } else {
-            current.right = new Node(value)
-            break
-          }
-        } else {
-          // go left
-          if (current.left) {
-            current = current.left
-            depth++
-          } else {
-            current.left = new Node(value)
-            break
-          }
-        }
-      }
-      this._checkBalance(path)
-      // check for imbalance here
-      // insert rotate logic here
+      this.root.add(value)
     }
-  }
-  _checkBalance(path) {
-    for (let i = path.length - 1; i >= 0; i--) {
-      const depth = {}
-      const node = path[i]
-      console.log(node)
-      if (node.left) depth.left = this._checkDepth(node.left)
-      if (node.right) depth.right = this._checkDepth(node.right)
-      // console.log(depth)
-      if (depth.right > 1) {
-        this._rotateRight(node)
-        break
-      }
-      if (depth.left > 1) {
-        this._rotateLeft(node)
-        break
-      }
-      // if balance num is over 1 rotate
-    }
-    // start at end of path, check balance
-    // ? recursive call
-  }
-  _rotateRight(node) {
-    if (node.right.left) {
-      const left = node.right.left.value
-      node.right.left.value = node.right.value
-      node.right.value = left
-
-      node.right.right = node.right.left
-      node.right.left = null
-    }
-    const temp = node.value
-    node.value = node.right.value
-    node.right.value = temp
-
-    node.left = node.right
-    node.right = node.right.right
-    node.left.right = null
-
-    // take node, swap value with first child
-    // then set self as left or right child
-    // take chi
-  }
-
-  _rotateLeft(node) {
-    if (node.left.right) {
-      const right = node.left.right.value
-      node.left.right.value = node.left.value
-      node.left.value = right
-
-      node.left.left = node.left.right
-      node.left.right = null
-    }
-    const temp = node.value
-    node.value = node.left.value
-    node.left.value = temp
-
-    node.right = node.left
-    node.left = node.left.left
-    node.right.left = null
-    // take node, swap value with first child
-    // then set self as left or right child
-    // take chi
-  }
-  _checkDepth(node) {
-    // checks depth of a diven node
-    if (!node.left && !node.right) return 1
-    if (!node.left) return this._checkDepth(node.right) + 1
-    if (!node.right) return this._checkDepth(node.left) + 1
-    const left = this._checkDepth(node.left)
-    const right = this._checkDepth(node.right)
-    return left < right ? right + 1 : left + 1
   }
   toObject() {
     return this.root
@@ -143,10 +168,27 @@ class Tree {
 class Node {
   constructor(value) {
     this.value = value
-    this.left = null
     this.right = null
+    this.left = null
+    this.height = 1
   }
-  // code also goes here
+  add(value) {
+    if (value < this.value) {
+      if (this.left) {
+        this.left.add(value)
+      } else {
+        this.left = new Node(value)
+      }
+      this.height = this.left.height + 1
+    } else {
+      if (this.right) {
+        this.right.add(value)
+      } else {
+        this.right = new Node(value)
+      }
+      this.height = this.right.height + 1
+    }
+  }
 }
 
 // unit tests
